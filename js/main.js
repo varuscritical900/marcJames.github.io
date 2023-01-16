df4216cb8f81cfd8be3dc0d4e5122354d5a1048c
varuscritical900 <varuscrtical900@gmail.com>
2023-01-16 09:12:57 +0800

    adding my css, bootstrap and js.

---
+++ js/main.js
@@ -0,0 +1,61 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () =>{
+    search.classList.toggle('active');
+    cart.classList.remove('active');
+    user.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let cart = document.querySelector('.cart');
+
+document.querySelector('#cart-icon').onclick = () =>{
+    cart.classList.toggle('active');
+    search.classList.remove('active');
+    user.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let user = document.querySelector('.user');
+
+document.querySelector('#user-icon').onclick = () =>{
+    user.classList.toggle('active');
+    search.classList.remove('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () =>{
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+    cart.classList.remove('active');
+    user.classList.remove('active');
+}
+
+// Navbar Scroll
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () =>{
+    header.classList.toggle('shadow', window.scrollY > 0);
+})
+
+
+// Swiper
+var swiper = new Swiper(".new-arrival", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
